f5a955940e2eab647be766e1e5f0e75c746457f3
mplibunao <markpaololibunao@gmail.com>
2017-07-20 00:48:55 +0800

    Started Show Single post by post id. Set up reducer to concat post fetch result to current state and mapStateToProps to filter down the state to be passed to the component to only include the post corresponding to match.params.id

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+export const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = `http://reduxblog.herokuapp.com/api`;
 const API_KEY = '?key=mplibunao';
@@ -18,14 +19,24 @@ export function fetchPosts(){
 export function createPost(values, callback){
   const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values)
     .then(()=> callback());
-  /*
+  
+  /* Error: body not passed in the post request
   const request = fetch(`${ROOT_URL}/posts${API_KEY}`, {
     method: 'POST',
-    body: values
-  });
+    body: JSON.stringify(values)
+  }).then(res => { console.log(res); callback(); } );
   */
   return {
     type: CREATE_POST,
+    payload: request
+  };
+}
+
+export function fetchPost(id){
+  //http://reduxblog.herokuapp.com/api/posts/5	
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+  return {
+    type: FETCH_POST,
     payload: request
   };
 }
